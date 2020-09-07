7995d8e675f17ba60cacfb4368d03b57afcaafa9
faithfulojebiyi <faithfulojebiyi@gmail.com>
2020-09-07 04:13:09 +0100

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+function resolveSrc (_path) {
+  return path.join(__dirname, _path)
+}
+// vue.config.js
+module.exports = {
+  lintOnSave: true,
+  sourceMap: true,
+  productionSourceMap: false,
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    resolve: {
+      alias: {
+        assets: resolveSrc('src/assets')
+      }
+    },
+    module: {
+      rules: [
+        ...{
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@vue/cli-plugin-babel/preset']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ]
+  }
+}
